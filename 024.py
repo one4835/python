1c86b78035a57d06a4eed344d45dac7c7ce3ba68
one4835 <57549928+one4835@users.noreply.github.com>
2019-12-01 16:36:42 +0900

    python

---
+++ 024.py
@@ -0,0 +1,3 @@
+string = "PYTHON"
+#print (string[5],string[4],string[3],string[2],string[1],string[0])
+print(string[::-1])
---
+++ 025.py
@@ -0,0 +1,2 @@
+phone_number = "010-1111-2222"
+print (phone_number[0:3], phone_number[4:8],phone_number[9:13])
