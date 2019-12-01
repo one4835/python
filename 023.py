e7ab9b020f18531437e1550febdd733d182735f3
one4835 <57549928+one4835@users.noreply.github.com>
2019-12-01 16:42:05 +0900

    Update 023.py

--- 023.py
+++ 023.py
@@ -4,4 +4,6 @@ sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 string = "홀짝홀짝홀짝"
-print (string[0],string[2],string[4])
+#print(string[0],string[2],string[4])
+print(string[::2]) #홀홀홀
+print(string[1::2]) #짝짝짝
